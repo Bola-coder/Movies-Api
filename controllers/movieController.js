22c32f5e2c4f78544cb5a22dddcb255294ec0627
Tesla <bolarinwaahmed22@gmail.com>
2021-12-06 19:31:54 +0100

    Refactoring the code to make it modular and reuseabse by moving the api features to utils

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,4 +1,5 @@
 const Movie = require("./../models/movieModel");
+const ApiFeatures = require("./../utils/apiFeatures");
 
 exports.createNewMovie = async (req, res) => {
   try {
@@ -19,48 +20,13 @@ exports.createNewMovie = async (req, res) => {
 
 exports.getAllMovies = async (req, res) => {
   try {
-    // BUILDING QUERY
-
-    // 1.) FILTERING DATA
-    const queryObj = { ...req.query };
-    console.log(queryObj);
-    const excludedFields = ["page", "sort", "limit", "fields"];
-    excludedFields.forEach((element) => delete queryObj[element]);
-
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    let query = Movie.find(JSON.parse(queryStr));
-
-    // 2. SORTING DATA
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(",").join(" ");
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort("price");
-    }
-
-    // LIMITING DATA
-    if (req.query.fields) {
-      const fields = req.query.fields.split(",").join(" ");
-      query = query.select(fields);
-    } else {
-      query = query.select("-__v");
-    }
-
-    // PAGINATION
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 5;
-    const skip = (page - 1) * limit;
-    query = query.skip(skip).limit(limit);
-    if (req.query.page) {
-      const numOfMovies = await Movie.countDocuments();
-      console.log(skip);
-      console.log(numOfMovies);
-      if (skip >= numOfMovies) throw "Page does not exist!!!";
-    }
-
     // EXECUTING QUERY
-    const movies = await query;
+    const features = new ApiFeatures(Movie.find(), req.query)
+      .filter()
+      .sort()
+      .fieldLimit()
+      .pagination();
+    const movies = await features.query;
 
     res.status(200).json({
       status: "success",
---
+++ utils/apiFeatures.js
@@ -0,0 +1,53 @@
+class ApiFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    // 1.) FILTERING DATA
+    const queryObj = { ...this.queryString };
+    console.log(queryObj);
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    excludedFields.forEach((element) => delete queryObj[element]);
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    // let query = Movie.find(JSON.parse(queryStr));
+    this.query = this.query.find(JSON.parse(queryStr));
+    return this;
+  }
+
+  sort() {
+    // 2. SORTING DATA
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(",").join(" ");
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort("price");
+    }
+    return this;
+  }
+
+  fieldLimit() {
+    // LIMITING DATA
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(",").join(" ");
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select("-__v");
+    }
+    return this;
+  }
+
+  pagination() {
+    // PAGINATION
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 5;
+    const skip = (page - 1) * limit;
+    this.query = this.query.skip(skip).limit(limit);
+    return this;
+  }
+}
+
+module.exports = ApiFeatures;
